705b9053b64fbf2abacc8abfa4cee9f04f833d7e
gelloguiam <acguiam@gmail.com>
2017-03-31 20:30:56 +0900

    integrated ui components

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET landing page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
 });
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
 
 module.exports = router;
