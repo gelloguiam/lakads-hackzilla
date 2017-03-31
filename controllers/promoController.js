a4de56860e27f06a740f8d9f86827dbdb00d5354
Matthew Marcos <matthewmarcos94@gmail.com>
2017-04-01 04:51:28 +0800

    Finished controllers

---
+++ controllers/promoController.js
@@ -0,0 +1,25 @@
+exports.add = (req, res, next) => {
+    res.send('Export.add');
+};
+
+
+exports.edit = (req, res, next) => {
+
+    let id;
+
+    if(req.params.id) {
+        id = req.params.id
+    }
+    res.send(`Export.put ${id}`);
+};
+
+
+exports.find = (req, res, next) => {
+    res.send('Export.find');
+
+};
+
+
+exports.findOne = (req, res, next) => {
+    res.send('Export.add');
+};
