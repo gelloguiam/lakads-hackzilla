6ac7990c159107515337d29dd46ec2ffcda383eb
gelloguiam <acguiam@gmail.com>
2017-04-01 05:52:17 +0900

    added partials for dynamic data

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -5,15 +5,15 @@
  */
 module.exports = {
     login: function(req, res, next) {
-        res.render('login', { title: 'Express' });
+        res.render('login', { title: 'Login', username: 'Hackzilla' });
     },
 
     home: function(req, res, next) {
-        res.render('home', { title: 'Express' });
+        res.render('home', { title: 'Home', username: 'Hackzilla' });
     },
 
     map: function(req, res, next) {
-        res.render('map', { title: 'Express' });
+        res.render('map', { title: 'Map', username: 'Hackzilla' });
     },
 
     corstest: function(req, res, next) {
