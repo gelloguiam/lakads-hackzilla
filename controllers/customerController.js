18007e938bdb8e1c9615e0d8de4d47ca158ede8f
mamerisawesome <atmalmer23@gmail.com>
2017-04-01 12:47:29 +0800

    Added auth to customer routes

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -11,168 +11,196 @@ module.exports = {
      * customerController.list()
      */
     list: function (req, res) {
-        customerModel.find(function (err, customers) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting customer.',
-                    error: err
-                });
-            }
-            return res.json(customers);
-        });
+        if (!req.user) {
+            return res.redirect('/');
+        } else {
+            customerModel.find(function (err, customers) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Error when getting customer.',
+                        error: err
+                    });
+                }
+                return res.json(customers);
+            });
+        }
     },
 
     /**
      * customerController.list_twitter_creds()
      */
     listTwitterCreds: function (req, res) {
-        var customersData = { 
-            __v: false,
-            _id: false,
-            access_token: false,
-            interests: false,
-            isDeleted: false
-        };
+        if (!req.user) {
+            return res.redirect('/');
+        } else {
+            var customersData = { 
+                __v: false,
+                _id: false,
+                access_token: false,
+                interests: false,
+                isDeleted: false
+            };
 
-        User.find({}, customersData, function (err, customers) {
-            if (err) return next(err);
-            else res.json(customers);
-        });
+            User.find({}, customersData, function (err, customers) {
+                if (err) return next(err);
+                else res.json(customers);
+            });
+        }
     },
 
     /**
      * customerController.showById()
      */
-    showById: function (req, res) {
-        var id = req.params.id;
-        customerModel.findOne({_id: id}, function (err, customer) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting customer.',
-                    error: err
-                });
-            }
-            if (!customer) {I
-                return res.status(404).json({
-                    message: 'No such customer'
-                });
-            }
-            return res.json(customer);
-        });
+    show: function (req, res) {
+        if (!req.user) {
+            return res.redirect('/');
+        } else {
+            var id = req.params.id;
+            customerModel.findOne({_id: id}, function (err, customer) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Error when getting customer.',
+                        error: err
+                    });
+                }
+                if (!customer) {I
+                    return res.status(404).json({
+                        message: 'No such customer'
+                    });
+                }
+                return res.json(customer);
+            });
+        }
     },
 
     /**
      * customerController.showByHandle()
      */
-    showByHandle: function (req, res) {
-        var twitter_handle = req.params.twitter_handle;
-        customerModel.findOne({twitter_handle: twitter_handle}, function (err, customer) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting customer.',
-                    error: err
-                });
-            }
-            if (!customer) {I
-                return res.status(404).json({
-                    message: 'No such customer'
-                });
-            }
-            return res.json(customer);
-        });
+    showAllHandle: function (req, res) {
+        if (!req.user) {
+            return res.redirect('/');
+        } else {
+            var customersData = { 
+                __v: false,
+                _id: false,
+                name: false,
+                twitter_handle: false,
+                interests: false,
+                isDeleted: false
+            };
+
+            User.find({}, customersData, function (err, customers) {
+                if (err) return next(err);
+                else res.json(customers);
+            });
+        }
     },
 
     /**
      * customerController.showByToken()
      */
-    showByToken: function (req, res) {
-        var token = req.params.token;
-        customerModel.findOne({token: token}, function (err, customer) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting customer.',
-                    error: err
-                });
-            }
-            if (!customer) {I
-                return res.status(404).json({
-                    message: 'No such customer'
-                });
-            }
-            return res.json(customer);
-        });
+    showAllToken: function (req, res) {
+        if (!req.user) {
+            return res.redirect('/');
+        } else {
+            var customersData = { 
+                __v: false,
+                _id: false,
+                name: false,
+                accesstoken: false,
+                interests: false,
+                isDeleted: false
+            };
+
+            User.find({}, customersData, function (err, customers) {
+                if (err) return next(err);
+                else res.json(customers);
+            });
+        }
     },
 
     /**
      * customerController.create()
      */
     create: function (req, res) {
-        var customer = new customerModel({
-			name : req.body.name,
-			twitter_handle : req.body.twitter_handle,
-			access_token : req.body.access_token,
-        });
+        if (!req.user) {
+            return res.redirect('/');
+        } else {
+            var customer = new customerModel({
+                name : req.body.name,
+                twitter_handle : req.body.twitter_handle,
+                access_token : req.body.access_token,
+            });
 
-        customer.save(function (err, customer) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when creating customer',
-                    error: err
-                });
-            }
-            return res.status(201).json(customer);
-        });
+            customer.save(function (err, customer) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Error when creating customer',
+                        error: err
+                    });
+                }
+                return res.status(201).json(customer);
+            });
+        }
     },
 
     /**
      * customerController.update()
      */
     update: function (req, res) {
-        var id = req.params.id;
-        customerModel.findOne({_id: id}, function (err, customer) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting customer',
-                    error: err
-                });
-            }
-            if (!customer) {
-                return res.status(404).json({
-                    message: 'No such customer'
-                });
-            }
-
-            customer.name = req.body.name ? req.body.name : customer.name;
-			customer.twitter_handle = req.body.twitter_handle ? req.body.twitter_handle : customer.twitter_handle;
-			customer.access_token = req.body.access_token ? req.body.access_token : customer.access_token;
-			customer.interests = req.body.interests ? req.body.interests : customer.interests;
-			
-            customer.save(function (err, customer) {
+        if (!req.user) {
+            return res.redirect('/');
+        } else {
+            var id = req.params.id;
+            customerModel.findOne({_id: id}, function (err, customer) {
                 if (err) {
                     return res.status(500).json({
-                        message: 'Error when updating customer.',
+                        message: 'Error when getting customer',
                         error: err
                     });
                 }
+                if (!customer) {
+                    return res.status(404).json({
+                        message: 'No such customer'
+                    });
+                }
 
-                return res.json(customer);
+                customer.name = req.body.name ? req.body.name : customer.name;
+                customer.twitter_handle = req.body.twitter_handle ? req.body.twitter_handle : customer.twitter_handle;
+                customer.access_token = req.body.access_token ? req.body.access_token : customer.access_token;
+                customer.interests = req.body.interests ? req.body.interests : customer.interests;
+                
+                customer.save(function (err, customer) {
+                    if (err) {
+                        return res.status(500).json({
+                            message: 'Error when updating customer.',
+                            error: err
+                        });
+                    }
+
+                    return res.json(customer);
+                });
             });
-        });
+        }
     },
 
     /**
      * customerController.remove()
      */
     remove: function (req, res) {
-        var id = req.params.id;
-        customerModel.findByIdAndRemove(id, function (err, customer) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when deleting the customer.',
-                    error: err
-                });
-            }
-            return res.status(204).json();
-        });
+        if (!req.user) {
+            return res.redirect('/');
+        } else {
+            var id = req.params.id;
+            customerModel.findByIdAndRemove(id, function (err, customer) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Error when deleting the customer.',
+                        error: err
+                    });
+                }
+                return res.status(204).json();
+            });
+        }
     }
 };
